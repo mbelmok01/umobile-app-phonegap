add8e396e9ffd135a322829deab8296f1ab0866f
Belmokhtar <mohamed_el_amine.belmokhtar@univ-lr.fr>
2014-06-18 19:54:05 +0200

    Each user is identified by his login

--- src/js/src/service/Push.js
+++ src/js/src/service/Push.js
@@ -1,50 +1,34 @@
 /*global window:true, _:true, document:true, jQuery:true, umobile:true, config:true, console:true */
 (function ($, _, umobile, config) {
 
-    
-    umobile.push.init = function() {
-
-        if(umobile.app.stateModel.get('notification') === null)
-        {
-            umobile.push.register();
-        }
-    };
-
     umobile.push.register = function() {
 
-        var username = umobile.app.credModel.get('username');
         var pushConfig = {
             senderID: "974817829387",
-            pushServerURL: "http://10.13.3.240:8080/ag-push/",
+            pushServerURL: "http://192.168.0.36:8080/ag-push/",
             variantID: "83a837f9-d00b-4fae-9217-1e6b120774ea",
             variantSecret: "11f594bd-d634-444f-8ed8-899c84a54826",
-            alias: username
+            alias: "Mohamed"
         };
 
-        try {
 
-            push.register(onNotification, successHandler, errorHandler, pushConfig);
+        var statusList = $("body");
+        statusList.append('<li>deviceready event received</li>');
+
+        try {
+            statusList.append('<li>registering </li>');
+            console.log(pushConfig);
+            push.register(umobile.push.onNotification, umobile.push.successHandler, umobile.push.errorHandler, pushConfig);
         } catch (err) {
-            txt = "There was an error on this page.\n\n";
-            txt += "Error description: " + err.message + "\n\n";
-            alert(txt);
+          txt = "There was an error on this page.\n\n";
+          txt += "Error description: " + err.message + "\n\n";
+          alert(txt);
         }
     };
 
-    umobile.push.unregister = function() {
-        
-        push.unregister(successUnregister, errorHandler);
-    };
-
-    function successHandler() {
-        $("body").append('<li>success</li>');
-    }
-
-    function errorHandler(error) {
-        $("body").append('<li>error:' + error + '</li>');
-    }
-
-    function onNotification(e) {
+    umobile.push.onNotification = function (e) {
+    
+        console.log('entree dans onNotification');
         var statusList = $("body");
 
         // on android we could play the sound, if we add the Media plugin
@@ -63,7 +47,14 @@
         if (e.badge) {
             push.setApplicationIconBadgeNumber(successHandler, e.badge);
         }
-    }
+    };
 
+    umobile.push.successHandler = function (success) {
+        $("body").append('<li>success : ' + success + '</li>');
+    };
 
+    umobile.push.errorHandler = function (error) {
+        $("body").append('<li>error : ' + error + '</li>');
+    };
+    
 })(jQuery, _, umobile, config);
